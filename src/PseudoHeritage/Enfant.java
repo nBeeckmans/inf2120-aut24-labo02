7d206d0b7362bbcafdb9f13af3432451c0cbf9a7
nbeeckmans <nicolas.beeckmans@hotmail.com>
2024-09-16 16:39:49 -0400

    Ajout pseudo heritage

---
+++ src/PseudoHeritage/Enfant.java
@@ -0,0 +1,9 @@
+package PseudoHeritage;
+
+public class Enfant {
+    private Parent parent;
+
+    public Enfant() {
+        parent = new Parent(1,2);
+    }
+}
---
+++ src/PseudoHeritage/Parent.java
@@ -0,0 +1,8 @@
+package PseudoHeritage;
+
+public class Parent {
+
+    public Parent(int a, int b) {
+        System.out.println(a + b);
+    }
+}
